66175973783d49f031c2a33f6ef83499dc2c0798
zhongxi <zhongxi710@gmail.com>
2024-03-05 08:57:09 +0900

    3월5일 글쓰기 막힘

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -51,13 +53,16 @@ Route::post('/articles', function(Request $request) {
     $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
 
     //쿼리 준비
-    $stmt = $conn->prepare("INSERT INTO article (body, user_id VALUE (:body, :userId)");
+    $stmt = $conn->prepare("INSERT INTO article (body, user_id) VALUES (:body, :userId)");
 
     
     $body = $request->input('body'); //body만 가져올 때, request->all하면 배열형식으로 모든 정보를 다 가져옴, request->collect하면 collect로 가져옴(내용은 똑같이 배열로 되어있음)
     //쿼리 값을 설정
-    $stmt->bindValue(':body',$input['body']);
-    $stmt->bindValue(':userId', Auth::id());
+    $stmt->bindValue(':body', $input['body']);
+    $stmt->bindValue(':userId', Auth::id());    
+
+    // 실행
+    $stmt->execute();
 
     return 'hello';
 });
